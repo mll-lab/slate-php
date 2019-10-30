227d55ab3cbd46bb1896fb63395dca9387804f60
Benedikt Franke <benedikt.franke@mll.com>
2019-10-30 11:56:51 +0100

    Bundle the logic on how nodes are serialized into JSON into the AST objects themselves

---
+++ src/Model/JsonStringConvertible.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Prezly\Slate\Model;
+
+trait JsonStringConvertible
+{
+    /**
+     * @param string $json
+     * @return static
+     */
+    public static function fromJSON(string $json)
+    {
+        return static::jsonDeserialize(json_decode($json));
+    }
+
+    /**
+     * @return string
+     */
+    public function toJSON()
+    {
+        return json_encode(static::jsonSerialize());
+    }
+
+    /**
+     * @param \stdClass $object
+     * @return static
+     */
+    abstract public static function jsonDeserialize(\stdClass $object);
+
+    /**
+     * @return \stdClass
+     */
+    abstract public function jsonSerialize(): \stdClass;
+}
